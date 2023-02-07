3d8ff82557284adfc208a42b66c99049975cd153
Aaron Carrillo <carlos.carrillo.delgado.contractor@bbva.com>
2023-02-07 12:35:35 -0500

    PERAR-21012 loggers in rimacresponse

--- artifact/libraries/RBVDR211IMPL/src/main/java/com/bbva/rbvd/lib/r211/impl/RBVDR211Impl.java
+++ artifact/libraries/RBVDR211IMPL/src/main/java/com/bbva/rbvd/lib/r211/impl/RBVDR211Impl.java
@@ -240,6 +240,10 @@ public class RBVDR211Impl extends RBVDR211Abstract {
 			}
 
 			if(nonNull(rimacResponse)) {
+				LOGGER.info("rimacResponse => {}",rimacResponse);
+				LOGGER.info("rimacResponse - cuotasFinanciamiento => {}",rimacResponse.getPayload().getCuotasFinanciamiento());
+				LOGGER.info("rimacResponse - primaBrutaSinIGV => {}",rimacResponse.getPayload().getPrimaBrutaSinIGV());
+
 				Map<String, Object> argumentsRimacContractInformation = this.mapperHelper.getRimacContractInformation(rimacResponse, asoResponse.getData().getId());
 				argumentsRimacContractInformation.forEach(
 						(key, value) -> LOGGER.info("***** executeBusinessLogicEmissionPrePolicy - UpdateContract parameter {} with value: {} *****", key, value));
