efddcbdcfe5c9b6651ee4118e2cb32d0558a0647
Aaron <carlos.carrillo.delgado@bbva.com>
2024-04-09 11:55:25 -0500

    feat: PARSRBVD-1842 PARSRBVD-2240 Update changes with develop

--- artifact/libraries/RBVDR211IMPL/src/main/java/com/bbva/rbvd/lib/r211/impl/util/MapperHelper.java
+++ artifact/libraries/RBVDR211IMPL/src/main/java/com/bbva/rbvd/lib/r211/impl/util/MapperHelper.java
@@ -145,6 +145,9 @@ public class MapperHelper {
     private static final String N_VALUE = "N";
     private static final Long INDICATOR_INSPECTION_NOT_REQUIRED_VALUE = 0L;
     private static final Long INDICATOR_INSPECTION_REQUIRED_VALUE = 1L;
+    private static final Long NUMBER_QUOTAS_UNEMPLOYMENT = 1L;
+    private static final String FREE_PERIOD = "L";
+    private static final String MONTHLY_PERIOD = "MONTHLY";
     private static final String PAYMENT_METHOD_VALUE = "DIRECT_DEBIT";
     private static final String COLLECTION_STATUS_FIRST_RECEIPT_VALUE = "00";
     private static final String COLLECTION_STATUS_NEXT_VALUES = "02";
@@ -158,7 +161,6 @@ public class MapperHelper {
     private static final String TAG_ENDORSEE = "ENDORSEE";
     private static final String FIELD_SYSTEM = "SYSTEM";
     private static final String FIELD_EXTERNAL_CONTRACT = "EXTERNAL_CONTRACT";
-    private static final String FIELD_EXTERNAL_CONTRACT_HOST = "EXT";
     private static final String FIELD_INTERNAL_CONTRACT = "INTERNAL_CONTRACT";
     private static final String GMT_TIME_ZONE = "GMT";
     private static final String RUC_ID = "R";
@@ -620,7 +622,7 @@ public class MapperHelper {
 
         String productsCalculateValidityMonths = this.applicationConfigurationService.getDefaultProperty("products.modalities.only.first.receipt","");
         String  operacionGlossaryDesc = responseQueryGetRequiredFields.get(RBVDProperties.FIELD_OPERATION_GLOSSARY_DESC.getValue()).toString();
-        if("MONTHLY".equals(requestBody.getInstallmentPlan().getPeriod().getId()) &&
+        if(MONTHLY_PERIOD.equals(requestBody.getInstallmentPlan().getPeriod().getId()) &&
                 !(requestBody.getProductId().equals(RBVDProperties.INSURANCE_PRODUCT_TYPE_VIDA_EASYYES.getValue()) ||
                 requestBody.getProductId().equals(RBVDProperties.INSURANCE_PRODUCT_TYPE_VIDA_2.getValue()) ||
                 requestBody.getProductId().equals(RBVDProperties.INSURANCE_PRODUCT_TYPE_VIDA_3.getValue()) ||
@@ -858,8 +860,10 @@ public class MapperHelper {
 
         String  operacionGlossaryDesc = responseQueryGetRequiredFields.get(RBVDProperties.FIELD_OPERATION_GLOSSARY_DESC.getValue()).toString();
         if (Arrays.asList(productsCalculateValidityMonths.split(",")).contains(operacionGlossaryDesc)) {
-            financiamiento.setFrecuencia("L");
-            financiamiento.setNumeroCuotas(1L);
+            if(MONTHLY_PERIOD.equals(requestBody.getInstallmentPlan().getPeriod().getId())){
+                financiamiento.setFrecuencia(FREE_PERIOD);
+            }
+            financiamiento.setNumeroCuotas(NUMBER_QUOTAS_UNEMPLOYMENT);
         }
 
         List<FinanciamientoBO> financiamientoBOs = new ArrayList<>();
@@ -1998,7 +2002,7 @@ public class MapperHelper {
 
                 relationType.setId(loanNumberArray[0]);
 
-                contractDetails.setContractType(FIELD_EXTERNAL_CONTRACT_HOST);
+                contractDetails.setContractType(FIELD_EXTERNAL_CONTRACT);
                 contractDetails.setNumber(loanNumberArray[1]);
 
                 relatedContract.setRelationType(relationType);
